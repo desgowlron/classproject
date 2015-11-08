79065d846b818438d8fa8da0c4f293a72dd38030
desgowlron <desgowlron@yahoo.com>
2015-11-07 19:45:26 -0500

    Initial commit

---
+++ app/src/main/java/com/smith/myweatherapp/DetailsActivity.java
@@ -0,0 +1,53 @@
+package com.smith.myweatherapp;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+
+public class DetailsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_details);
+
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_details, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }else if(id == android.R.id.home){
+            finish();
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    public void btnHandle3(View view) {
+        Intent dispIntent = new Intent(this, DisplayActivity.class);
+        startActivity(dispIntent);
+    }
+
+    public void btnHandle4(View view) {
+        Intent locatIntent = new Intent(this, LocationActivity.class);
+        startActivity(locatIntent);
+    }
+}
---
+++ app/src/main/java/com/smith/myweatherapp/DisplayActivity.java
@@ -0,0 +1,49 @@
+package com.smith.myweatherapp;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+
+public class DisplayActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_display);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_display, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    public void btnHandle(View view) {
+        Intent locatIntent = new Intent(this,LocationActivity.class);
+        startActivity(locatIntent);
+    }
+
+    public void btnHandle2(View view) {
+        Intent detailIntent = new Intent(this, DetailsActivity.class);
+        startActivity(detailIntent);
+    }
+}
